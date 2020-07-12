63f71ea17b88884c8f07f60c77de06839d46fb02
y0092888 <ju-horn@web.de>
2020-07-12 17:30:47 +0200

    reCaptcha works

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 var express = require("express");
 var router = express.Router();
+require("dotenv").config();
 
 const fs = require("fs");
+const fetch = require("isomorphic-fetch");
+
 const { throws } = require("assert");
 
 const dataFile = require("./dataFile");
@@ -9,11 +12,17 @@ const dataFile = require("./dataFile");
 let users = dataFile.getUsers();
 const formIds = new Set();
 
-/* GET home page. */
+/*TODO: use process.env for this!*/
+
 router.get("/", function (req, res, next) {
   const id = getRandomId();
   formIds.add(id);
-  res.render("index", { title: "We were here", formId: id, users: users });
+  res.render("index", {
+    title: "We were here",
+    formId: id,
+    users: users,
+    googleReCaptcha: process.env.CAPTCHA_PUBLIC,
+  });
 });
 
 router.post("/data", function (req, res, next) {
@@ -25,6 +34,10 @@ router.post("/data", function (req, res, next) {
     res.write("Data send twice or id became invalid!");
     res.end();
   }
+});
+
+router.post("/test", function (req, res, next) {
+  handleSend(req, res);
 });
 
 function isRequestValid(req) {
@@ -51,5 +64,18 @@ function getRandomId() {
 function generateRandomNumber() {
   return Math.floor(Math.random() * 10000);
 }
+
+const handleSend = (req, res) => {
+  const secret_key = process.env.CAPTCHA_SECRET;
+  const token = req.body.token;
+  const url = `https://www.google.com/recaptcha/api/siteverify?secret=${secret_key}&response=${token}`;
+
+  fetch(url, {
+    method: "post",
+  })
+    .then((response) => response.json())
+    .then((google_response) => res.json({ google_response }))
+    .catch((error) => res.json({ error }));
+};
 
 module.exports = router;
